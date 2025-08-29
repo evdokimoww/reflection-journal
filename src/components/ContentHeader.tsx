a5ff9c0c812254d71e2ce47fbdc0c74c01de7d7f
aevdokimov <aevdokimov@t1-group.ru>
2025-08-29 18:06:47 +0300

    refactor header

--- src/components/ContentHeader.tsx
+++ src/components/ContentHeader.tsx
@@ -1,30 +1,45 @@
 "use client";
 
-import { Breadcrumb, Flex } from "@chakra-ui/react";
+import { Breadcrumb, Button, Flex } from "@chakra-ui/react";
 import { usePathname } from "next/navigation";
 import { BREADCRUMPS_PAGE_NAMES } from "@/shared/data/breadcrumbs.data";
-import { Fragment } from "react";
+import React, { Fragment } from "react";
+import { PAGES } from "@/config/pages.config";
+import Link from "next/link";
+
+interface IPath {
+  link: string;
+  name: string;
+}
 
 export default function ContentHeader() {
   const pathname = usePathname();
-  const path = pathname.split("/").filter(Boolean);
+  const path = pathname.split("/").reduce((acc, p) => {
+    if (p) {
+      acc.push({
+        link: p,
+        name:
+          BREADCRUMPS_PAGE_NAMES[p as keyof typeof BREADCRUMPS_PAGE_NAMES] || p,
+      });
+    }
+    return acc;
+  }, [] as IPath[]);
 
   return (
-    <Flex h="20" w="100%" mb="8" align="center">
-      <Breadcrumb.Root size="lg">
+    <Flex h="20" w="100%" mb="8" align="center" gap="4">
+      <Breadcrumb.Root size="lg" fontWeight="bold">
         <Breadcrumb.List>
           {path.length > 0 ? (
-            path.map((p: string, index) => {
+            path.map((p: IPath, index) => {
               return (
-                <Fragment key={p}>
-                  <Breadcrumb.Item>
-                    <Breadcrumb.Link href={`/${p}`}>
-                      {/*todo fix after adding entries*/}
-                      {BREADCRUMPS_PAGE_NAMES[
-                        p as keyof typeof BREADCRUMPS_PAGE_NAMES
-                      ] || p}
-                    </Breadcrumb.Link>
-                  </Breadcrumb.Item>
+                <Fragment key={p.link}>
+                  {index !== path.length - 1 ? (
+                    <Link href={`/${p.link}`}>
+                      <Breadcrumb.Item>{p.name}</Breadcrumb.Item>
+                    </Link>
+                  ) : (
+                    <Breadcrumb.CurrentLink>{p.name}</Breadcrumb.CurrentLink>
+                  )}
                   {index < path.length - 1 && <Breadcrumb.Separator />}
                 </Fragment>
               );
@@ -36,6 +51,13 @@ export default function ContentHeader() {
           )}
         </Breadcrumb.List>
       </Breadcrumb.Root>
+      {pathname === PAGES.ENTRIES && (
+        <Link href={PAGES.CREATE_ENTRY}>
+          <Button variant="surface" size="sm">
+            + Новая запись
+          </Button>
+        </Link>
+      )}
     </Flex>
   );
 }
