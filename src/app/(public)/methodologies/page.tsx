4f35f927589e87909dfaca4dc4a7cbeae69a9d51
Alexey Evdokimov <evdokimoww@gmail.com>
2026-01-14 18:09:17 +0300

    add pwa functionality

--- src/app/(public)/methodologies/page.tsx
+++ src/app/(public)/methodologies/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
+import { MethodologyItemDialog } from "@/components/methodologies/MethodologyItemDialog.tsx";
+import { Loader } from "@/components/public/Loader.tsx";
 import {
   useIsMethodologiesLoading,
   useMethodologies,
   useMethodologiesActions,
 } from "@/shared/stores/methodologies/hooks.ts";
+import { Avatar, Box, Button, Card, Dialog, Flex } from "@chakra-ui/react";
 import { useEffect } from "react";
-import { Loader } from "@/components/public/Loader.tsx";
-import { Avatar, Button, Card, Dialog, Flex } from "@chakra-ui/react";
-import { MethodologyItemDialog } from "@/components/methodologies/MethodologyItemDialog.tsx";
 
 export default function MethodologiesPage() {
   const { fetchMethodologies } = useMethodologiesActions();
@@ -22,15 +22,37 @@ export default function MethodologiesPage() {
   return isLoading ? (
     <Loader />
   ) : (
-    <Flex gap="4" wrap="wrap">
+    <Flex
+      gap="4"
+      wrap="wrap"
+      flexDirection={{ base: "column", md: "row" }}
+      alignItems={{ base: "center", md: "none" }}
+    >
       {methodologies.map((methodology) => (
-        <Card.Root width="320px" key={methodology.id}>
+        <Card.Root width={{ base: "100%", md: "300px" }} key={methodology.id}>
           <Card.Body gap="2">
-            <Avatar.Root size="lg" shape="rounded">
+            <Avatar.Root
+              size="lg"
+              shape="rounded"
+              display={{ base: "none", md: "flex" }}
+            >
               <Avatar.Fallback name={methodology.title} />
             </Avatar.Root>
             <Card.Title mt="2">{methodology.title}</Card.Title>
-            <Card.Description>{methodology.short_description}</Card.Description>
+            <Box h="70px">
+              <Card.Description
+                maxH="70px"
+                overflow="hidden"
+                textOverflow="ellipsis"
+                style={{
+                  display: "-webkit-box",
+                  WebkitLineClamp: 3,
+                  WebkitBoxOrient: "vertical",
+                }}
+              >
+                {methodology.short_description}
+              </Card.Description>
+            </Box>
           </Card.Body>
           <Card.Footer justifyContent="flex-end">
             <Dialog.Root size="xl">
